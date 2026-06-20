218f9e93de98a6df4a700db9d5b1b39a61721b43
takktakk417 <s2201016@sendai-nct.jp>
2026-06-21 02:42:22 +0900

    mediapipeとったてきなあれ

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,33 @@
 const CACHE_NAME = "blink-monitor-v1";
 
 const FILES = [
+  "./",
+  "./index.html",
 
-    "./",
-    "./index.html",
+  "./css/style.css",
 
-    "./css/style.css",
+  "./js/app.js",
+  "./js/blink.js",
+  "./js/camera.js",
+  "./js/face.js",
+  "./js/storage.js",
+  "./js/ui.js",
+  "./js/chart.js",
 
-    "./js/app.js",
-    "./js/blink.js",
-    "./js/camera.js",
-    "./js/face.js",
-    "./js/storage.js",
-    "./js/ui.js",
-    "./js/chart.js",
+  "./assets/models/face_landmarker.task",
 
-    "./assets/models/face_landmarker.task",
-
-    "./assets/mediapipe/vision_bundle.mjs",
-    "./assets/mediapipe/wasm/vision_wasm_internal.wasm"
+  "./assets/mediapipe/vision_bundle.mjs",
+  "./assets/mediapipe/wasm/vision_wasm_internal.wasm"
 ];
+
+const SAFE_FILES = FILES.filter(f =>
+  !f.includes("mediapipe")
+);
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(async cache => {
-
-      for (const file of FILES) {
-        try {
-          const res = await fetch(file);
-          if (!res.ok) throw new Error("bad response");
-          await cache.put(file, res);
-        } catch (e) {
-          console.warn("skip cache:", file);
-        }
-      }
-
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(SAFE_FILES))
   );
 });
 
